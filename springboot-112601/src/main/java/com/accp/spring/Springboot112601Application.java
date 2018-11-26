f176b5af17a2d8d7715e917e79666d2861d816ef
373376253 <373376253@users.noreply.github.com>
2018-11-26 14:49:55 +0800

    第一次提交

---
+++ springboot-112601/src/main/java/com/accp/spring/Springboot112601Application.java
@@ -0,0 +1,12 @@
+package com.accp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot112601Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot112601Application.class, args);
+	}
+}
